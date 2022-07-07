9aba8c187ef3dc73c563cfc20d4ab7e84bf349e0
RAVINTEJA <ravintej02@gmail.com>
2022-07-07 16:05:18 +0530

    Corrected errors of statis files and succesfully deployed

--- adaptive_emergence/urls.py
+++ adaptive_emergence/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home')
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
